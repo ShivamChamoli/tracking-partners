9674ac34c7effd67a23b241ef92c1b13464b0cb1
Shivam Chamoli <shchamol@shchamol-mac.local>
2018-06-07 10:18:50 -0400

    added bootstrap to section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../css/Section.css';
+import { Row, Col } from 'react-bootstrap';
 
 export default class Content extends React.Component {
     constructor(props) {
@@ -8,10 +9,16 @@ export default class Content extends React.Component {
     render() {
         return (
           <div className="partner-section">
-              <div className="partner-header"/>
+              <Row className="partner-header">
+                        <Col xs={12} md={12}>hi</Col>
+              </Row>
+              <Row className="partners">
+                        <Col xs={12} md={12}> {this.props.id} </Col>
+              </Row>
+              {/*<div className="partner-header"/>
               <div className="partners">
                 {this.props.id}
-              </div>
+        </div>*/}
           </div>
         );
     }
